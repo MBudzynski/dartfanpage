8bfa22b338c40c830c3a4e84806a7f1249f237a1
MBudzynski <michalbu92@gmail.com>
2021-07-11 11:39:14 +0200

    first commit

---
+++ src/main/java/com/example/dartfanpage/DartfanpageApplication.java
@@ -0,0 +1,13 @@
+package com.example.dartfanpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DartfanpageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DartfanpageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dartfanpage/DartfanpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dartfanpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DartfanpageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
